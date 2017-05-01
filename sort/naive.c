3f63294c34577d9956f978173df4584cb1492b4a
Igor Martire <igorx94@gmail.com>
2017-05-01 12:14:22 -0300

    :art: Organize repo

--- sort/naive.c
+++ sort/naive.c
@@ -10,32 +10,43 @@ int main(int argc, char** argv) {
   int list[] = {2,5,8,5,8,7,3,2,8,11};
 
   int tag = 0;
-  MPI_Status status;
-  int list_len = 10;
+  int list_len = sizeof(list)/sizeof(int);
+  double t1, t2;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-  if(my_rank == 0){
+  if (my_rank == 0) {
+    t1 = MPI_Wtime();
+  }
+
+  int sorted_index = get_sorted_index(list, list_len, my_rank);
+  if(my_rank == 0) {
     int *sorted_list = (int *)malloc(list_len*sizeof(int));
-    int sorted_index = get_sorted_index(list, list_len, my_rank);
-    sorted_list[sorted_index] = list[my_rank];
+    sorted_list[sorted_index] = list[0];
+
     for(int i = 1; i < np; i++){
       int received_sorted_index;
-      MPI_Recv(&received_sorted_index, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(&received_sorted_index, 1, MPI_INT, i, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       sorted_list[received_sorted_index] = list[i];
     }
+
     for(int i = 0; i < list_len; i++){
       printf("%d ", sorted_list[i]);
     }
     printf("\n");
+    
     free(sorted_list);
-  }else{
-    int sorted_index = get_sorted_index(list, list_len, my_rank);
+  }
+  else {
     MPI_Send(&sorted_index, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
   }
 
+  if (my_rank == 0) {
+    t2 = MPI_Wtime();
+    printf( "Elapsed time is %f\n", t2 - t1 );
+  }
   MPI_Finalize();
 }
 
